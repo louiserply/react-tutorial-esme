cfe33752830f7327178eba4a036e4d8368a7cb33
Pierre-Aymeric MASSE <pierreaymeric.masse@orange.com>
2016-11-23 22:08:41 +0100

    props with class component example

--- src/index.js
+++ src/index.js
@@ -2,28 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    componentWillMount(){
-       console.log("Component will be mount")
-    }
-
-    componentDidMount(){
-        console.log("Component is mount")
-    }
 
     render(){
         return (
-            <h1> hello </h1>
+            <div>
+                <h1> {this.props.title} </h1>
+                <small> {this.props.children} </small>
+            </div>
         )
     }
 
-    componentWillUnmount() {
-        console.log("Component will be unmount")
-    }
-
-    componentDidUnmount() {
-        console.log("Component is unmount")
-    }
 }
 
-ReactDOM.render(<App />, document.querySelector('.app'));
+App.propTypes = {
+  title: React.PropTypes.string
+};
+
+ReactDOM.render(<App title="React tutorial"> Debut </App>, document.querySelector('.app'));
 
