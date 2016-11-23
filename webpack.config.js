709182d3ea6f3c45e9c4390dad86b2d9ef2148eb
Pierre-Aymeric MASSE <pierreaymeric.masse@orange.com>
2016-11-23 21:29:59 +0100

    function component example

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loaders: 'babel',
+                loader: 'babel',
                 query: {
                     presets: ['es2015', 'react', 'stage-2']
                 }
