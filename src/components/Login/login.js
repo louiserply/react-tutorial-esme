dd9374dda952b34d833c36b4874306583c817df1
Pierre-Aymeric MASSE <pierreaymeric.masse@orange.com>
2016-11-24 17:45:08 +0100

    Login form with Semantic UI components

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import AuthService from '../../services/authentification';
-import { Button, Input } from 'semantic-ui-react'
+import { Button, Input, Form } from 'semantic-ui-react'
 
 export default class Login extends React.Component {
-
     constructor() {
         super();
         this.state = {
@@ -12,35 +11,45 @@ export default class Login extends React.Component {
         };
     }
 
+
     handleChange(e) {
         this.setState({[e.target.name]: e.target.value});
     }
 
-     login(e) {
-         e.preventDefault();
-         if(AuthService.login(this.state.user, this.state.password)){
-             alert('Success !');
-             //redirect to protected route (Cf step 4)
-             //and store the login token (here without store)
-         } else {
-             alert('Fail !');
-         }
+    login(e) {
+        e.preventDefault();
+        if (AuthService.login(this.state.user, this.state.password)) {
+            alert('Success !');
+            //redirect to protected route (Cf step 4)
+            //and store the login token (here without store)
+        } else {
+            alert('Fail !');
+        }
     }
 
     render() {
+        const loginStyle = {
+            container : {
+                marginTop : "10px",
+            }
+        }
 
         return (
-            <form role="form">
-                <div className="form-group">
-                    <Input type="text" name="user" value={this.state.user}
-                           placeholder="Username"
-                           onChange={this.handleChange.bind(this)}  />
-                    <Input type="password" name="password" value={this.state.password}
-                           placeholder="Password"
-                           onChange={this.handleChange.bind(this)} />
-                </div>
-                <Button type="submit" onClick={this.login.bind(this)}>Submit</Button>
-            </form>
+            <div className="ui middle aligned center aligned grid" style ={loginStyle.container}>
+                <Form>
+                    <Form.Field>
+                        <Input type="text" name="user" value={this.state.user}
+                               placeholder="Username"
+                               onChange={this.handleChange.bind(this)}/>
+                    </Form.Field>
+                    <Form.Field>
+                        <Input type="password" name="password" value={this.state.password}
+                               placeholder="Password"
+                               onChange={this.handleChange.bind(this)}/>
+                    </Form.Field>
+                    <Button type="submit" onClick={this.login.bind(this)}>Submit</Button>
+                </Form>
+            </div>
         );
     }
 }
